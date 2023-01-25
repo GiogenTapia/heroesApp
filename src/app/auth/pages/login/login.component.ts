87b942cc393971a64b2df6e17bd186f347e9f60f
Giovanni Tapia <ryukaj1_@hotmail.com>
2023-01-25 17:07:50 -0600

    agregado de proteccion de rutas#

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  
+  ngOnInit(): void {
+   
+  }
+
+  constructor(private _router : Router,
+              private _authService : AuthService){
+
+  }
+
+  login(){
+
+    this._authService.login().subscribe(
+      resp=>{
+        if (resp.id) {
+          this._router.navigate(['./heroes'])
+        }
+      }
+    );
+  
+  }
+
 
 }
